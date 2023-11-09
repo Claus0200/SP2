6f7b53a8eff1cfdd1fbf2c791d3abdb6c2a55cfd
claus0200 <claus.peter.joergensen@gmail.com>
2023-11-09 17:03:28 +0100

    Update

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
         FleetOfCars fleetOfCars = new FleetOfCars();
 
 
-        //fleetOfCars.addCar(new GasolineCar("Audi","A6","A2C B46",4,15));
-        //fleetOfCars.addCar(new DieselCar("Bmw", "W6", "CFK 6A8",4,12,true));
+        fleetOfCars.addCar(new GasolineCar("Audi","A6","A2C B46",4,15));
+        fleetOfCars.addCar(new DieselCar("Bmw", "W6", "CFK 6A8",4,12,true));
         fleetOfCars.addCar(new ElectricCar("Toyota", "bz4x", "SEE YA",2,64,400));
 
         System.out.println(fleetOfCars);
